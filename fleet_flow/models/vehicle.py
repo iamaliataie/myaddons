de8632a78dd3fba475897cdc8d823f649674ccd9
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-10-01 10:15:24 +0430

    ineherit fleet.vehicle model

--- fleet_flow/models/vehicle.py
+++ fleet_flow/models/vehicle.py
@@ -3,10 +3,8 @@
 from odoo import models, fields, api
 
 
-class Vehicle(models.Model):
-    _name = 'fleetflow.vehicle'
-    _description = 'Fleet Flow Vehicle'
-    # _inherit = 'fleet.vehicle'
+class FleetVehicle(models.Model):
+    _inherit = 'fleet.vehicle'
     
-    name = fields.Char()
+    comapny = fields.Char()
 
